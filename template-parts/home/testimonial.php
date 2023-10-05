f6cac0cab96f0eab375fd3a789562e160a2903d9
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2023-10-05 16:48:38 +0200

    finished testimonial section

--- template-parts/home/testimonial.php
+++ template-parts/home/testimonial.php
@@ -6,28 +6,28 @@
         <div class="testimonial__grid__item testimonial__grid__item--1">
             <p class="description"><i>“Andries used his wealth of experience and understanding of how humans learn best to foster a culture of growth in the Web Operations team that he led during my time at 2U. Always being keen to innovate and push the boundaries of what is possible made Andries a key component of a very successful team - he would be an absolute asset to any brand looking to drive hyper growth in the digital space.”</i></p>
             <div class="details">
-                <img src="" alt="" class="portrait">
-                <h6 class="name">Name Surname</h6>
-                <span class="role">head leader of job</span>
-                <img src="" alt="" class="company">
+                <img src="<?php echo get_theme_file_uri('images/testimonial-ricky.png'); ?>" alt="" class="portrait">
+                <h6 class="name">Ricky Hendricks</h6>
+                <span class="role">Industry Leader</span>
+                <img src="<?php echo get_theme_file_uri('images/testimonial-google.png'); ?>" alt="" class="company">
             </div>  
         </div>
         <div class="testimonial__grid__item testimonial__grid__item--1">
             <p class="description">“<i>I had the privilege of managing Andries Bester for eight months. His passion for Conversion Rate Optimization (CRO) and dedication to this field are evident in his work and the results he consistently delivers. One of Dries' most commendable qualities is his focus on people and team culture. He understands the importance of a positive and collaborative work environment and goes above and beyond to foster this within his team...”</i></p>
             <div class="details">
-                <img src="" alt="" class="portrait">
-                <h6 class="name">Name Surname</h6>
-                <span class="role">head leader of job</span>
-                <img src="" alt="" class="company">
+                <img src="<?php echo get_theme_file_uri('images/testimonial-pieter.png'); ?>" alt="" class="portrait">
+                <h6 class="name">Pieter Malan</h6>
+                <span class="role">Senior Director, Software Engineering</span>
+                <img src="<?php echo get_theme_file_uri('images/testimonial-edx.png'); ?>" alt="" class="company">
             </div>  
         </div>
         <div class="testimonial__grid__item testimonial__grid__item--1">
             <p class="description"><i>“Dries is someone I trust implicitly. He’s hugely passionate, driven and responsible. He’s the definition of a self-starter, and supremely capable, but he’s also humble, always willing to take accountability, and constantly looking to learn and improve. He has a clear vision of what he wants to achieve, and he leads through authenticity, vulnerability, organisational skill and a genuine desire to do the very best for his team...”</i></p>
             <div class="details">
-                <img src="" alt="" class="portrait">
-                <h6 class="name">Name Surname</h6>
-                <span class="role">head leader of job</span>
-                <img src="" alt="" class="company">
+                <img src="<?php echo get_theme_file_uri('images/testimonial-tim.png'); ?>" alt="" class="portrait">
+                <h6 class="name">Tim Withers</h6>
+                <span class="role">Vice President, Digital</span>
+                <img src="<?php echo get_theme_file_uri('images/testimonial-2u.png'); ?>" alt="" class="company">
             </div>  
         </div>
         
