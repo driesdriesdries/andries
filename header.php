515e39c8b3a6a2a6a90380e74d35ce57a63144d6
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2023-11-03 15:59:51 +0200

    added GA below the head

--- header.php
+++ header.php
@@ -22,6 +22,15 @@
 	
 	<?php wp_head(); ?>
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6X5GJQ5JQM"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6X5GJQ5JQM');
+</script>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
