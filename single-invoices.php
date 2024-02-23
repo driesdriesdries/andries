b8da730a1ef1d9e1ac8000208e4f6d54c2bd38cd
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-23 13:13:05 +0200

    mvp of frontend

--- single-invoices.php
+++ single-invoices.php
@@ -9,6 +9,78 @@
 
 get_header(); ?>
 
+<div class="invoice-container">
+    <div class="invoice">
+        <div class="header">
+            <h2>Invoice</h2>
+            <p>Andries Bester</p>
+            <p>13 Firdale Avenue</p>
+            <p>Gardens</p>
+            <p>Cape Town</p>
+            <p>Mobile: +27712914643</p>
+            <p><a href="www.andriesbester.com">andriesbester.com</a></p>
+        </div>
+        <div class="details">
+            <div class="left">
+                <h4>Bill To:</h4>
+                <p>SANTS</p>
+            </div>
+            <div class="right">
+                <h4>Invoice Number: 2</h4>
+                <p>Invoice Date: February 19, 2024</p>
+                <p>Payment Due: March 14, 2024</p>
+                <p>Amount Due: R10 000</p>
+            </div>
+        </div>
+        <div class="table">
+            <table>
+                <thead>
+                    <tr>
+                    <th>Items</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>Sants Retainer</td>
+                    <td>20</td>
+                    <td>R500</td>
+                    <td>R10,000</td>
+                    </tr>
+                    <!-- Additional rows can be added here -->
+                    <tr>
+                    <td>Sants Retainer</td>
+                    <td>20</td>
+                    <td>R500</td>
+                    <td>R10,000</td>
+                    </tr>
+
+                    <tr>
+                    <td>Sants Retainer</td>
+                    <td>20</td>
+                    <td>R500</td>
+                    <td>R10,000</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="total-container">
+            <span class="label">Total:</span><span class="amount">R10,000</span>
+            <br>
+            <span class="label">Amount Due:</span><span class="amount">R10,000</span>
+        </div>
+        <div class="footer">
+            <h4>Notes</h4>
+            <p>Account Holder: AG Bester</p>
+            <p>Bank: FNB</p>
+            <p>Account Number: 912309210930</p>
+            <p>Branch Code: 250665</p>
+        </div>
+    </div>
+</div>
+
 <h1>Invoice Details</h1>
 
 <?php while ( have_posts() ) : the_post(); ?>
